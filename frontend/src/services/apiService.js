2d2de1eca524adf142195cd1359ee221510eb5b9
Ma-Eum <cl.gachelin@gmail.com>
2025-02-06 16:25:12 +0100

    Correction de getUserActivity et ajout de getUserAverageSessions

--- frontend/src/services/apiService.js
+++ frontend/src/services/apiService.js
@@ -3,9 +3,7 @@ import axios from "axios";
 const BASE_URL = "http://localhost:3000";
 
 /**
- * Récupère les informations d'un utilisateur par son ID
- * @param {number} userId - L'ID de l'utilisateur (12 ou 18)
- * @returns {Promise}
+ * Récupère les informations d'un utilisateur
  */
 export const getUserData = async (userId) => {
     try {
@@ -19,10 +17,21 @@ export const getUserData = async (userId) => {
 
 /**
  * Récupère l'activité quotidienne d'un utilisateur
- * @param {number} userId - L'ID de l'utilisateur (12 ou 18)
- * @returns {Promise}
  */
 export const getUserActivity = async (userId) => {
+    try {
+        const response = await axios.get(`${BASE_URL}/user/${userId}/activity`);
+        return response.data.data;
+    } catch (error) {
+        console.error("Erreur lors de la récupération des activités :", error);
+        return null;
+    }
+};
+
+/**
+ * Récupère les sessions moyennes d'un utilisateur
+ */
+export const getUserAverageSessions = async (userId) => {
     try {
         const response = await axios.get(`${BASE_URL}/user/${userId}/average-sessions`);
         return response.data.data;
