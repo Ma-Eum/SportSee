a746c2fafe7a8d594b9a01a70ced792588c65ec5
Ma-Eum <cl.gachelin@gmail.com>
2025-02-26 23:14:03 +0100

    erreur d'url du bakcend corriger

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,2 @@
 export const USE_MOCK_DATA = true; // 🚀 Change à `false` pour utiliser l'API réelle
-export const API_BASE_URL = "http://localhost:5173/api"; // 🚀 Change l'URL selon ton backend
+export const API_BASE_URL = "http://localhost:3000/api"; // 🚀 URL du backend
